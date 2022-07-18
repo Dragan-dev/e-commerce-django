2dfb01eda375f0030b551920c3f00e332915f190
Dragan-dev <dragansucevic5@gmail.com>
2022-07-18 17:09:20 +0200

    Adition test

--- articles/models.py
+++ articles/models.py
@@ -1,13 +1,13 @@
 from django.db.models.signals import pre_save, post_save
 from django.db import models
 from django.utils import timezone
-from django.utils.text import slugify
-import random
+from .utils import slugify_instance_title
+
 
 
 class Article(models.Model):
     title = models.CharField(max_length=250)
-    slug = models.SlugField(unique=True,blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -18,26 +18,6 @@ class Article(models.Model):
         # if self.slug is None:
         # self.slug=slugify(self.title)
         super().save(*args, **kwargs)
-
-
-
-
-def slugify_instance_title(instance, save=False, new_slug=None):
-        if new_slug is not None:
-            slug = new_slug
-        else:
-            slug = slugify(instance.title)
-
-        qs = Article.objects.filter(slug=slug).exclude(id=instance.id)
-
-        if qs.exists:
-            rand_int = random.randint(100, 300)
-            slug = f"{slug}-{rand_int}"
-
-        instance.slug = slug
-        if save:
-            instance.save()
-        return instance
 
 
 def article_pre_save(sender, instance, *args, **kwargs):
--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,37 @@
+from .utils import slugify
 from django.test import TestCase
 
 # Create your tests here.
+from . models import Article
+
+
+class ArticleTestCase(TestCase):
+
+    def setUp(self):
+        self.num_of_articles = 5
+        for i in range(self.num_of_articles):
+            Article.objects.create(
+                title=' Testing case title', content='Testing case content')
+
+    def test_qs_exists(self):
+        qs = Article.objects.all()
+        self.assertTrue(qs.exists())
+
+    def test_qs_count(self):
+        qs = Article.objects.all()
+        self.assertEqual(qs.count(), self.num_of_articles)
+
+    def test_hw_slug(self):
+        obj = Article.objects.all().order_by("id").first()
+        title = obj.title
+        slug = obj.slug
+        slugified_title = slugify(title)
+        self.assertEqual(slug, slugified_title)
+
+    def test_hw_slug(self):
+        qs = Article.objects.exclude(slug__iexact__= 'hello world')
+        for obj in qs:
+            title = obj.title
+            slug = obj.slug
+            slugified_title = slugify(title)
+            self.assertNotEquals(slug, slugified_title) 
