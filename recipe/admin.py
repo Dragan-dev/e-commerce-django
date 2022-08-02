7b00fda836feb984566e35b0b01bb9002ac9c282
Dragan-dev <dragansucevic5@gmail.com>
2022-08-02 19:29:02 +0200

    defininig recipe field on admin side

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Recipe, RecipeIngredient
 
-# Register your models here.
+
+User = get_user_model()
+admin.site.unregister(User)
+
+
+class RecipeInLine(admin.StackedInline):
+    model = Recipe
+    extra = 0
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [RecipeInLine]
+    list_display = ['username']
+
+
+admin.site.register(User, UserAdmin)
+
+
+class RecipeIngredientInLine(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+    # fields = ['name', 'quantity', 'unit', 'directions']
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInLine]
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+
+admin.site.register(Recipe, RecipeAdmin)
