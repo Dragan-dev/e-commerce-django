5dbf5abd9bab655241af7c9923da3c7e76f1e9f7
Dragan-dev <dragansucevic5@gmail.com>
2022-08-02 19:27:19 +0200

    addition user raw id field in admin model

--- articles/admin.py
+++ articles/admin.py
@@ -6,5 +6,7 @@ from . models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display=['id','title','timestamp','updated','slug']
     search_fields=['title','content']
+    raw_id_fields = ['user']
+
 
 admin.site.register(Article , ArticleAdmin)
